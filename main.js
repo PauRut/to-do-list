94ea17c726cf4cfb6cd521c40ee2483299025bf4
Paulina Rutkowska <polciaalicious@gmail.com>
2022-08-07 21:20:19 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+// window.addEventListener('load',  () => {
+//     const form = document.querySelector('#new-task-form');
+//     const input = document.querySelector('#new-task-input');
+//     const list_el = document.querySelector('#tasks');
+ 
+//     form.addEventListener('submit', (e) => {
+//         e.preventDefault();
+
+//         const task = input.value;
+//       }
+
+//
+// Will try to fix it this week.
